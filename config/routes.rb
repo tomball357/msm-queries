a373dc22744dbd6a0cf2231b13be83b1ad77fcc7
tomball357 <tball@chicagobooth.edu>
2025-03-15 01:29:14 +0000

    save

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
 end
+
+
+get("/directors/youngest", { :controller => "directors", :action => "youngest" })
+
+get("/directors/:the_id", { :controller => "directors", :action => "show" })
